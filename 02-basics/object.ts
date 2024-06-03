40d20541f6ff50f050b3f1a0e235772a50ae9c72
Its-utsav <utsavdhimmar987@gmail.com>
2024-06-03 21:25:09 +0530

    about type in ts

--- 02-basics/object.ts
+++ 02-basics/object.ts
@@ -25,3 +25,47 @@ function CreateUser(user: User): User {
 }
 
 CreateUser({ name: "Utsav", age: 18, isMale: true });
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+let point1: Point = {
+  x: 10,
+  y: 12,
+};
+
+type Name = string;
+
+let anyName: Name = "Utsav";
+
+type Person = {
+  name: string;
+  age: number;
+  isMale: boolean;
+};
+
+type Student = Person & {
+  course: string;
+};
+
+let Student1: Student = {
+  name: "Utsav",
+  age: 18,
+  isMale: true,
+  course: "Bca",
+};
+
+type Id = number | string;
+
+let userId: Id = 123;
+userId = "abc123";
+
+type Greet = (name: string) => string;
+
+const greeting: Greet = (name) => `Hello ${name}`;
+
+console.log(greeting("utsav"));
+
+export {};
