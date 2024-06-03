d3ce24eb078a4aaa6e34c0c3f7ae90b5d843b583
Its-utsav <utsavdhimmar987@gmail.com>
2024-06-03 17:09:03 +0530

    init commit and set up for ts code

---
+++ 01-intro/hello.ts
@@ -0,0 +1,28 @@
+// let user = {
+//   name: "Utsav",
+//   age: 17,
+// };
+
+// console.log("Hello World");
+
+// console.log(user.name);
+
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+
+// add(1, 2);
+
+// let myName: string = "Utsav"; // now only suggest string methods
+// // console.log(myName.charAt(2));
+
+// let num = 12;
+// let isMale = true;
+// console.log(num);
+
+let hero: string;
+function getHero() {
+  return "Thor";
+}
+hero = getHero();
+export {};
